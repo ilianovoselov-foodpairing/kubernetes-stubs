ec5458ca2f14c12fec28da7c5a078db7ccba1f12
Ilia Novoselov <ilya.novoselov@gmail.com>
2021-10-06 17:36:03 +0500

    Added __all__

--- codegen/base/kubernetes-stubs/client/__init__.pyi
+++
@@ -1,5 +0,0 @@
-from kubernetes.client.api_client import ApiClient
-from kubernetes.client.configuration import Configuration
-from kubernetes.client.exceptions import (ApiException, ApiKeyError,
-                                          ApiTypeError, ApiValueError,
-                                          OpenApiException)
