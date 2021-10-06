8d18b0da002b4af47f74e96b7f1c6237ddbaf39f
Ilia Novoselov <ilya.novoselov@gmail.com>
2021-10-06 14:01:12 +0500

    Fixed api imports

--- kubernetes-stubs/client/api/__init__.pyi
+++ kubernetes-stubs/client/api/__init__.pyi
@@ -1,77 +1,83 @@
-from kubernetes.client.api.admissionregistration import \
+from kubernetes.client.api.admissionregistration_api import \
     AdmissionregistrationApi
-from kubernetes.client.api.admissionregistration_v1 import \
+from kubernetes.client.api.admissionregistration_v1_api import \
     AdmissionregistrationV1Api
-from kubernetes.client.api.admissionregistration_v1beta1 import \
+from kubernetes.client.api.admissionregistration_v1beta1_api import \
     AdmissionregistrationV1beta1Api
-from kubernetes.client.api.apiextensions import ApiextensionsApi
-from kubernetes.client.api.apiextensions_v1 import ApiextensionsV1Api
-from kubernetes.client.api.apiextensions_v1beta1 import ApiextensionsV1beta1Api
-from kubernetes.client.api.apiregistration import ApiregistrationApi
-from kubernetes.client.api.apiregistration_v1 import ApiregistrationV1Api
-from kubernetes.client.api.apiregistration_v1beta1 import \
+from kubernetes.client.api.apiextensions_api import ApiextensionsApi
+from kubernetes.client.api.apiextensions_v1_api import ApiextensionsV1Api
+from kubernetes.client.api.apiextensions_v1beta1_api import \
+    ApiextensionsV1beta1Api
+from kubernetes.client.api.apiregistration_api import ApiregistrationApi
+from kubernetes.client.api.apiregistration_v1_api import ApiregistrationV1Api
+from kubernetes.client.api.apiregistration_v1beta1_api import \
     ApiregistrationV1beta1Api
-from kubernetes.client.api.apis import ApisApi
-from kubernetes.client.api.apps import AppsApi
-from kubernetes.client.api.apps_v1 import AppsV1Api
-from kubernetes.client.api.auditregistration import AuditregistrationApi
-from kubernetes.client.api.auditregistration_v1alpha1 import \
+from kubernetes.client.api.apis_api import ApisApi
+from kubernetes.client.api.apps_api import AppsApi
+from kubernetes.client.api.apps_v1_api import AppsV1Api
+from kubernetes.client.api.auditregistration_api import AuditregistrationApi
+from kubernetes.client.api.auditregistration_v1alpha1_api import \
     AuditregistrationV1alpha1Api
-from kubernetes.client.api.authentication import AuthenticationApi
-from kubernetes.client.api.authentication_v1 import AuthenticationV1Api
-from kubernetes.client.api.authentication_v1beta1 import \
+from kubernetes.client.api.authentication_api import AuthenticationApi
+from kubernetes.client.api.authentication_v1_api import AuthenticationV1Api
+from kubernetes.client.api.authentication_v1beta1_api import \
     AuthenticationV1beta1Api
-from kubernetes.client.api.authorization import AuthorizationApi
-from kubernetes.client.api.authorization_v1 import AuthorizationV1Api
-from kubernetes.client.api.authorization_v1beta1 import AuthorizationV1beta1Api
-from kubernetes.client.api.autoscaling import AutoscalingApi
-from kubernetes.client.api.autoscaling_v1 import AutoscalingV1Api
-from kubernetes.client.api.autoscaling_v2beta1 import AutoscalingV2beta1Api
-from kubernetes.client.api.autoscaling_v2beta2 import AutoscalingV2beta2Api
-from kubernetes.client.api.batch import BatchApi
-from kubernetes.client.api.batch_v1 import BatchV1Api
-from kubernetes.client.api.batch_v1beta1 import BatchV1beta1Api
-from kubernetes.client.api.batch_v2alpha1 import BatchV2alpha1Api
-from kubernetes.client.api.certificates import CertificatesApi
-from kubernetes.client.api.certificates_v1beta1 import CertificatesV1beta1Api
-from kubernetes.client.api.coordination import CoordinationApi
-from kubernetes.client.api.coordination_v1 import CoordinationV1Api
-from kubernetes.client.api.coordination_v1beta1 import CoordinationV1beta1Api
-from kubernetes.client.api.core import CoreApi
-from kubernetes.client.api.core_v1 import CoreV1Api
-from kubernetes.client.api.custom_objects import CustomObjectsApi
-from kubernetes.client.api.discovery import DiscoveryApi
-from kubernetes.client.api.discovery_v1beta1 import DiscoveryV1beta1Api
-from kubernetes.client.api.events import EventsApi
-from kubernetes.client.api.events_v1beta1 import EventsV1beta1Api
-from kubernetes.client.api.extensions import ExtensionsApi
-from kubernetes.client.api.extensions_v1beta1 import ExtensionsV1beta1Api
-from kubernetes.client.api.flowcontrolApiserver import FlowcontrolApiserverApi
-from kubernetes.client.api.flowcontrolApiserver_v1alpha1 import \
+from kubernetes.client.api.authorization_api import AuthorizationApi
+from kubernetes.client.api.authorization_v1_api import AuthorizationV1Api
+from kubernetes.client.api.authorization_v1beta1_api import \
+    AuthorizationV1beta1Api
+from kubernetes.client.api.autoscaling_api import AutoscalingApi
+from kubernetes.client.api.autoscaling_v1_api import AutoscalingV1Api
+from kubernetes.client.api.autoscaling_v2beta1_api import AutoscalingV2beta1Api
+from kubernetes.client.api.autoscaling_v2beta2_api import AutoscalingV2beta2Api
+from kubernetes.client.api.batch_api import BatchApi
+from kubernetes.client.api.batch_v1_api import BatchV1Api
+from kubernetes.client.api.batch_v1beta1_api import BatchV1beta1Api
+from kubernetes.client.api.batch_v2alpha1_api import BatchV2alpha1Api
+from kubernetes.client.api.certificates_api import CertificatesApi
+from kubernetes.client.api.certificates_v1beta1_api import \
+    CertificatesV1beta1Api
+from kubernetes.client.api.coordination_api import CoordinationApi
+from kubernetes.client.api.coordination_v1_api import CoordinationV1Api
+from kubernetes.client.api.coordination_v1beta1_api import \
+    CoordinationV1beta1Api
+from kubernetes.client.api.core_api import CoreApi
+from kubernetes.client.api.core_v1_api import CoreV1Api
+from kubernetes.client.api.custom_objects_api import CustomObjectsApi
+from kubernetes.client.api.discovery_api import DiscoveryApi
+from kubernetes.client.api.discovery_v1beta1_api import DiscoveryV1beta1Api
+from kubernetes.client.api.events_api import EventsApi
+from kubernetes.client.api.events_v1beta1_api import EventsV1beta1Api
+from kubernetes.client.api.extensions_api import ExtensionsApi
+from kubernetes.client.api.extensions_v1beta1_api import ExtensionsV1beta1Api
+from kubernetes.client.api.flowcontrolApiserver_api import \
+    FlowcontrolApiserverApi
+from kubernetes.client.api.flowcontrolApiserver_v1alpha1_api import \
     FlowcontrolApiserverV1alpha1Api
-from kubernetes.client.api.logs import LogsApi
-from kubernetes.client.api.networking import NetworkingApi
-from kubernetes.client.api.networking_v1 import NetworkingV1Api
-from kubernetes.client.api.networking_v1beta1 import NetworkingV1beta1Api
-from kubernetes.client.api.node import NodeApi
-from kubernetes.client.api.node_v1alpha1 import NodeV1alpha1Api
-from kubernetes.client.api.node_v1beta1 import NodeV1beta1Api
-from kubernetes.client.api.policy import PolicyApi
-from kubernetes.client.api.policy_v1beta1 import PolicyV1beta1Api
-from kubernetes.client.api.rbacAuthorization import RbacAuthorizationApi
-from kubernetes.client.api.rbacAuthorization_v1 import RbacAuthorizationV1Api
-from kubernetes.client.api.rbacAuthorization_v1alpha1 import \
+from kubernetes.client.api.logs_api import LogsApi
+from kubernetes.client.api.networking_api import NetworkingApi
+from kubernetes.client.api.networking_v1_api import NetworkingV1Api
+from kubernetes.client.api.networking_v1beta1_api import NetworkingV1beta1Api
+from kubernetes.client.api.node_api import NodeApi
+from kubernetes.client.api.node_v1alpha1_api import NodeV1alpha1Api
+from kubernetes.client.api.node_v1beta1_api import NodeV1beta1Api
+from kubernetes.client.api.policy_api import PolicyApi
+from kubernetes.client.api.policy_v1beta1_api import PolicyV1beta1Api
+from kubernetes.client.api.rbacAuthorization_api import RbacAuthorizationApi
+from kubernetes.client.api.rbacAuthorization_v1_api import \
+    RbacAuthorizationV1Api
+from kubernetes.client.api.rbacAuthorization_v1alpha1_api import \
     RbacAuthorizationV1alpha1Api
-from kubernetes.client.api.rbacAuthorization_v1beta1 import \
+from kubernetes.client.api.rbacAuthorization_v1beta1_api import \
     RbacAuthorizationV1beta1Api
-from kubernetes.client.api.scheduling import SchedulingApi
-from kubernetes.client.api.scheduling_v1 import SchedulingV1Api
-from kubernetes.client.api.scheduling_v1alpha1 import SchedulingV1alpha1Api
-from kubernetes.client.api.scheduling_v1beta1 import SchedulingV1beta1Api
-from kubernetes.client.api.settings import SettingsApi
-from kubernetes.client.api.settings_v1alpha1 import SettingsV1alpha1Api
-from kubernetes.client.api.storage import StorageApi
-from kubernetes.client.api.storage_v1 import StorageV1Api
-from kubernetes.client.api.storage_v1alpha1 import StorageV1alpha1Api
-from kubernetes.client.api.storage_v1beta1 import StorageV1beta1Api
-from kubernetes.client.api.version import VersionApi
+from kubernetes.client.api.scheduling_api import SchedulingApi
+from kubernetes.client.api.scheduling_v1_api import SchedulingV1Api
+from kubernetes.client.api.scheduling_v1alpha1_api import SchedulingV1alpha1Api
+from kubernetes.client.api.scheduling_v1beta1_api import SchedulingV1beta1Api
+from kubernetes.client.api.settings_api import SettingsApi
+from kubernetes.client.api.settings_v1alpha1_api import SettingsV1alpha1Api
+from kubernetes.client.api.storage_api import StorageApi
+from kubernetes.client.api.storage_v1_api import StorageV1Api
+from kubernetes.client.api.storage_v1alpha1_api import StorageV1alpha1Api
+from kubernetes.client.api.storage_v1beta1_api import StorageV1beta1Api
+from kubernetes.client.api.version_api import VersionApi
